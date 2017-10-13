f062ad2f078848deeb67dd57a547da23d1ba4bcd
Pontus Persson <pontus.alexander@gmail.com>
2017-10-14 01:01:05 +0200

    Add speed limit.

--- public/js/traits/Go.js
+++ public/js/traits/Go.js
@@ -1,18 +1,33 @@
 import {Trait} from '../Entity.js';
 
+function inhibit(value, max) {
+
+}
+
+
 export default class Go extends Trait {
     constructor() {
         super('go');
 
         this.dir = 0;
-        this.acceleration = 400;
+        this.acceleration = 600;
+        this.resistance = 1/5000;
 
         this.distance = 0;
         this.heading = 1;
     }
 
     update(entity, deltaTime) {
+        const drag = this.resistance * entity.vel.x * Math.abs(entity.vel.x);
+
         entity.vel.x += this.acceleration * deltaTime * this.dir;
+        entity.vel.x += -drag;
+
+        if (entity.vel.x > this.speedLimit) {
+            entity.vel.x = this.speedLimit;
+        } else if (entity.vel.x < -this.speedLimit) {
+            entity.vel.x = -this.speedLimit;
+        }
 
         if (this.dir) {
             this.heading = this.dir;
