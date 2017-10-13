aabd2c442e876544c3c09e28062936fa09be401a
Pontus Persson <pontus.alexander@gmail.com>
2017-10-14 00:58:49 +0200

    Add acceleration to Mario.

--- public/js/traits/Go.js
+++ public/js/traits/Go.js
@@ -5,15 +5,14 @@ export default class Go extends Trait {
         super('go');
 
         this.dir = 0;
-        this.speed = 6000;
-
+        this.acceleration = 400;
 
         this.distance = 0;
         this.heading = 1;
     }
 
     update(entity, deltaTime) {
-        entity.vel.x = this.speed * this.dir * deltaTime;
+        entity.vel.x += this.acceleration * deltaTime * this.dir;
 
         if (this.dir) {
             this.heading = this.dir;
